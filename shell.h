af4235ad16243bfe467b61ad4c48ae0714330d18
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-19 13:18:39 +0100

    Added function prototypes of the _strlen, _strcpy, and _strcmp functions, and two libriaries

--- shell.h
+++ shell.h
@@ -8,12 +8,17 @@
 #include <sys/wait.h>
 #define DELIMITER " \n"
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 /*#include <signal.h>*/
 
 char *read_line(void);
 char *duplicate(char *buff);
 char **tokenize(char *str, char *buff, char **token_array);
 void exec_func(char **token_array, char **environ);
+char *_strncpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *str);
 /*void sigint_handler(int signum);*/
 
 #endif /* #ifndef SHELL_H */
