919ad94624c02347533b00aa7d7760cd8615a4f1
stephen_adah <mailstephenreal@gmail.com>
2023-08-25 04:09:07 -0700

    i introduced access function inside

--- 00-execute_absolute_path.c
+++ 00-execute_absolute_path.c
@@ -10,25 +10,34 @@ void execute_absolute_path(char *args, char **token_array, char **environ)
 {
 	pid_t child_pid;
 	int status;
-	ssize_t a;
+	ssize_t a, x;
 
-	child_pid = fork();
-	if (child_pid == -1)
+	x = access(args, X_OK);
+	if (x == 0)
 	{
-		perror("fork function at absolute path failed");
-		exit(errno);
-	}
-	else if (child_pid == 0)
-	{
-		a = execve(args, token_array, environ);
-		if (a == -1)
+		child_pid = fork();
+		if (child_pid == -1)
 		{
-			error_message(args, token_array);
+			perror("fork function at absolute path failed");
 			exit(errno);
 		}
+		else if (child_pid == 0)
+		{
+			a = execve(args, token_array, environ);
+			if (a == -1)
+			{
+				perror("execve failed at execute_absolute_path");
+				exit(errno);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
 	}
-	else
+	if (x == -1)
 	{
-		wait(&status);
+		error_message(args, token_array);
+		exit(errno);
 	}
 }
