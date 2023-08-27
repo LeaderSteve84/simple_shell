1a6e5cf0ba777c909dfaf327cecc56589d07f538
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-27 23:31:02 +0100

    fix

--- 7-more_functions.c
+++ 7-more_functions.c
@@ -103,10 +103,6 @@ int exit_status(char **token_array)
 
 	if (token_array[1] == NULL)
 	{
-		if (errno != 0)
-		{
-			return(errno);
-		}
 		return (0);
 	}
 	if (_strcmp(token_array[1], "HBTN") == 0)
